9ec55d54dc2cbb19a302ea5c2ba6cf4d7c5ddea6
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-06-23 17:49:31 -0300

    Feito componente Rating para renderizar a nota do filme no MovieCard, feito a checagem de tipo do elemento recebido atraves da prop

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,18 @@
 import { shape, string, number } from 'prop-types';
 import React, { Component } from 'react';
+import Rating from './Rating';
 
 class MovieCard extends Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline, imagePath } = movie;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <div>
         <img src={ imagePath } alt={ `imagem do filme ${title}` } />
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
+        <Rating rating={ rating } />
       </div>
     );
   }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,19 @@
-// implement Rating component here
+import { number } from 'prop-types';
+import React, { Component } from 'react';
+
+class Rating extends Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <div>
+        <p className="rating">{rating}</p>
+      </div>
+    );
+  }
+}
+
+Rating.propTypes = {
+  rating: number.isRequired,
+};
+
+export default Rating;

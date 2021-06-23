b5fd06c364fc37563f6d5cafbdb51e9123133d26
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-06-23 15:32:49 -0300

    feito componente Header com o texto Movie Cards Library como titulo principal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
-      Hora de codar! 🚀✍️👨‍💻👩‍💻💪
+      <Header />
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,13 @@
-// Iniciando o projeto 🚀
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="header-container">
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
